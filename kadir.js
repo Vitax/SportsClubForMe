5e643dd6bbddaece4052033b08cc816ca0d1763b
Caglar Oezel <caglar.oezel@googlemail.com>
2015-03-21 14:36:35 +0100

    Cleanup

--- kadir.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('app', []);
-
-app.controller('SportsClubForMe',
-    function SportsClubForMe($scope) {
-        console.log("muh");
-        $scope.bla = "caglar";
-    }
-);
