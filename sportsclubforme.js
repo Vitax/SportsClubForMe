1581016f190252991353af79212f3610d5002fcc
caglar <caglar.oezel@googlemail.com>
2015-04-09 20:40:02 +0200

    marks are set according to the search terms, searching for postcodes is possible also get properly updated when looking for new terms or when deleting complete fixed performance
    Did some stupid iterating and called the set function for my service in a loop which resulted in a an exponential call which resulted in the crash / overload
    
    TODO: Add a function that shows all clubs like in 1 km radius of a give address
    TODO: Show club information in the marks

--- sportsclubforme.js
+++ sportsclubforme.js
@@ -1,18 +1,5 @@
 var app = angular.module('app', []);
 
-app.service('dataService', ['$rootScope', function ($rootScope) {
-    var data = null;
-
-    this.get = function () {
-        return data;
-    }
-
-    this.set = function (value) {
-        data = value;
-        $rootScope.$broadcast('onData');
-    }
-}]);
-
 app.service('geoDataService', ['$rootScope', function ($rootScope) {
     var searchValues = null;
 
@@ -26,18 +13,17 @@ app.service('geoDataService', ['$rootScope', function ($rootScope) {
     }
 }]);
 
-app.controller('ClubData', ['$scope', '$http', 'dataService', 'geoDataService', function ($scope, $http, dataService, geoDataService) {
+app.controller('ClubData', ['$scope', '$http', 'geoDataService', function ($scope, $http, geoDataService) {
 
-    $scope.data = null;
     $scope.searchCriteria = null;
-    $scope.results = {};
+    $scope.data = null;
+    $scope.keys = {};
 
     $http({
         method: 'GET',
         url: "assets/data/SportClubForMe_Working.json"
     }).success(function (data) {
         $scope.data = data;
-        dataService.set($scope.data);
     }).error(function (error) {
         console.log(error);
     });
@@ -54,45 +40,30 @@ app.controller('ClubData', ['$scope', '$http', 'dataService', 'geoDataService', 
     }
 
     $scope.searchClub = function () {
-        //$scope.results = {};
-
         var s = $scope.searchCriteria.toLowerCase();
+        if (s == '') {
+            geoDataService.set([]);
+            return;
+        }
 
+        $scope.keys = {};
         $scope.data.clubdata.forEach(function (entry) {
             if (entry.clubname.toLowerCase().indexOf(s) > -1 || entry.postcode.indexOf(s) > -1) {
-                var keys = Object.keys($scope.results);
-
-                if (keys.indexOf(entry.clubname) > -1 || keys.indexOf(entry.postcode) > -1) {
-                    $scope.results[entry.clubname].push(entry);
-                } else {
-                    $scope.results[entry.clubname] = [entry];
-                }
-
-                geoDataService.set($scope.results);
-
-                // push values of the jason without sorting
-                //$scope.results.push(entry);
+                $scope.keys[entry.clubname] = entry;
             }
         });
-        //display the $result array in the console
-        //console.log($scope.results);
+        geoDataService.set($scope.keys);
     }
+
 }]);
 
 
-app.controller('MapCtrl', ['$scope', '$http', 'dataService', 'geoDataService', function ($scope, $http, dataService, geoDataService) {
-
-
-    //$scope.$on('onData', function () {
-    //    $scope.cloneData = dataService.get();
-    //
-    //    $scope.initMarks();
-    //});
+app.controller('MapCtrl', ['$scope', '$http', 'geoDataService', function ($scope, $http, geoDataService) {
 
     $scope.$on('onUpdate', function () {
         $scope.clubResults = geoDataService.get();
 
-        $scope.initMarks();
+        initMarks();
     });
 
     var berlinLatLng = new google.maps.LatLng(52.50, 13.34);
@@ -117,26 +88,29 @@ app.controller('MapCtrl', ['$scope', '$http', 'dataService', 'geoDataService', f
         content: contentString
     });
 
-    //add marker to the map
-    $scope.initMarks = function () {
-
-        angular.forEach($scope.clubResults, function (value, key) {
-            for (var i = 0; i < value.length; i++) {
-                var clubLatLng = new google.maps.LatLng(value[i].position.lat,
-                    value[i].position.lng);
-
-                var marker = new google.maps.Marker({
-                    position: clubLatLng,
-                    map: map
-                });
-            }
-        });
+    var markers = [];
+    var setAllMap = function (map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+        markers = [];
     }
 
-    var deleteMarker = function (value) {
-        var marker = value;
+    //add marker to the map
+    var initMarks = function () {
+        setAllMap(null);
 
-        marker.setMap(null);
+        angular.forEach($scope.clubResults, function (value, key) {
+            var clubLatLng = new google.maps.LatLng(value.position.lat,
+                value.position.lng);
+
+            var marker = new google.maps.Marker({
+                position: clubLatLng,
+                map: map
+            });
+
+            markers.push(marker);
+        });
     }
 }]);
 
