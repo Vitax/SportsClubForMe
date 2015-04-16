2eda73e14ccac35be74ebd25bf7146fbcfe79715
Vitax <caglar.oezel@googlemail.com>
2015-04-16 22:25:00 +0200

    minor fixes and cleanup

--- sportsclubforme.js
+++ sportsclubforme.js
@@ -49,7 +49,8 @@ app.controller('DataCtrl', ['$scope', '$http', 'geoDataService', function ($scop
     var checkData = function () {
         $scope.data.clubdata.forEach(function (entry) {
             if (entry.postcode == undefined || entry.address == undefined || entry.phonenumber == undefined
-                || entry.mailaddress == undefined || entry.webpage == undefined || entry.latlng == undefined) {
+                || entry.mailaddress == undefined || entry.webpage == undefined || entry.latlng == undefined
+                || entry.district == undefined) {
                 console.log('Clubname: ' + entry.clubname);
                 console.log('Clublink: ' + entry.clublink);
             }
@@ -57,37 +58,27 @@ app.controller('DataCtrl', ['$scope', '$http', 'geoDataService', function ($scop
     }
 
     var levenshteinDistance = function (a, b) {
-        if (a == b) return 0;
-        if (a.length == 0) return b;
-        if (b.length == 0) return a;
 
-        var vector1 = [b.length + 1];
-        var vector2 = [b.length + 1];
+        var cost = 0;
+        if (a == b) return;
+        var vectorA = [a.length];
+        var vectorB = [b.length];
+
+        for (var i = 0; i < vectorA.length; i++) {
+            for (var j = 0; j < vectorB.length;) {
+                if (vectorA[i] != vectorB[j] && vectorA[i + 1] != vectorB[j]) {
+                    cost += 1;
+                } else if (vectorA[i] == vectorB[j]) {
+                    j++;
+                }
+            }
+        }
+
+        return cost;
     }
 
     $scope.searchClub = function () {
         var s = $scope.searchCriteria.toLowerCase();
-
-        // variables for the districts with their corresponding postcodes
-        var charlottenburg_wilmersdorf = [10585, 10587, 10589, 10623, 10625, 10627, 10629, 10707, 10709, 10711, 10713, 10715,
-            10717, 10719, 10777, 13627, 14050, 14052, 14053, 14055, 14057, 14059, 14193, 14197, 14199];
-        var friedrichshain_kreuzberg = [10178, 10179, 10243, 10245, 10247, 10249, 10317, 10961, 10963, 10965, 10967, 10969, 10997,
-            10999];
-        var lichtenberg = [10315, 10317, 10318, 10319, 10365, 10367, 10369, 13051, 13053, 13055, 13057, 13059];
-        var mitte = [10115, 10117, 10119, 10178, 10179, 10551, 10553, 10555, 10557, 10559, 10785, 10787, 13347, 13349, 13351,
-            13353, 13355, 13357, 13359, 13407, 13409];
-        var marzahn_hellersdorf = [12619, 12621, 12623, 12627, 12629, 12679, 12681, 12683, 12685, 12687, 12689];
-        var neukoelln = [12043, 12045, 12047, 12049, 12051, 12053, 12055, 12057, 12059, 12347, 12349, 12351, 12353, 12355, 12357, 12359];
-        var pankow = [10119, 10247, 10249, 10369, 10405, 10407, 10409, 10435, 10437, 10439, 13051, 13086, 13088, 13089, 13125,
-            13127, 13129, 13156, 13158, 13159, 13187, 13189];
-        var reinickendorf = [13403, 13405, 13407, 13409, 13435, 13437, 13439, 13465, 13467, 13469, 13503, 13505, 13507, 13509];
-        var spandau = [13581, 13583, 13585, 13587, 13589, 13591, 13593, 13595, 13597, 13599, 13629, 14052, 14089];
-        var steglitz_zehlendorf = [12157, 12161, 12163, 12165, 12167, 12169, 12203, 12205, 12207, 12209, 12247, 12249, 14109,
-            14129, 14163, 14165, 14167, 14169, 14193, 14195];
-        var tempelhof_schoeneberg = [10777, 10779, 10781, 10783, 10787, 10789, 10823, 10825, 10827, 10829, 12099, 12101, 12103,
-            12105, 12107, 12109, 12157, 12159, 12161, 12169, 12249, 12277, 12279, 12305, 12307, 12309, 14197];
-        var treptow_koepenick = [12435, 12437, 12439, 12459, 12487, 12489, 12524, 12526, 12527, 12555, 12557, 12559, 12587, 12589];
-
 
         if (s == '') {
             geoDataService.set([]);
@@ -96,15 +87,13 @@ app.controller('DataCtrl', ['$scope', '$http', 'geoDataService', function ($scop
 
         $scope.keys = {};
         $scope.data.clubdata.forEach(function (entry) {
-            if (entry.clubname.toLowerCase().indexOf(s) > -1 || entry.postcode.toLowerCase().indexOf(s) > -1
-                || entry.disctricts.toLowerCase().indexOf(s) > -1) {
-                $scope.keys[entry.clubname] = entry;
+                if (entry.clubname.toLowerCase().indexOf(s) > -1 || entry.district.toLowerCase().indexOf(s) > -1 || entry.postcode.toLowerCase().indexOf(s) > -1) {
+                    $scope.keys[entry.clubname] = entry;
+                }
             }
-        });
-
+        );
         geoDataService.set($scope.keys);
     }
-
 }]);
 
 
