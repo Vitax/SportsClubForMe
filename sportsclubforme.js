8fce83e6ad7249942fb79fef9b4fc03eeed93865
Vitax <caglar.oezel@googlemail.com>
2015-04-17 16:56:08 +0200

    added functionality to click the html objects and open a info window corresponding to it added functionality to the html to support the html object calls adapted the css fixed some data sets in the json file

--- sportsclubforme.js
+++ sportsclubforme.js
@@ -2,24 +2,24 @@ var app = angular.module('app', []);
 
 app.service('geoDataService', ['$rootScope', function ($rootScope) {
     var searchValues = null;
-    var zoomValue = 10;
+    var clubObject = null;
 
-    this.get = function () {
+    this.getClubData = function () {
         return searchValues;
     }
 
-    this.set = function (sportClubs) {
+    this.setClubData = function (sportClubs) {
         searchValues = sportClubs;
         $rootScope.$broadcast('onUpdate');
     }
 
-    this.getZoom = function () {
-        return zoomValue;
+    this.getObject = function () {
+        return clubObject;
     }
 
-    this.setZoom = function (value) {
-        zoomValue = value;
-        $rootScope.$broadcast('zoomChanged');
+    this.setObject = function (clickedObject) {
+        clubObject = clickedObject;
+        $rootScope.$broadcast('objectClicked');
     }
 }]);
 
@@ -57,31 +57,35 @@ app.controller('DataCtrl', ['$scope', '$http', 'geoDataService', function ($scop
         });
     }
 
-    var levenshteinDistance = function (a, b) {
-
-        var cost = 0;
-        if (a == b) return;
-        var vectorA = [a.length];
-        var vectorB = [b.length];
-
-        for (var i = 0; i < vectorA.length; i++) {
-            for (var j = 0; j < vectorB.length;) {
-                if (vectorA[i] != vectorB[j] && vectorA[i + 1] != vectorB[j]) {
-                    cost += 1;
-                } else if (vectorA[i] == vectorB[j]) {
-                    j++;
-                }
-            }
-        }
-
-        return cost;
+    $scope.clickedObject = function (clickedObject) {
+        geoDataService.setObject(clickedObject);
     }
+
+    // String comparison tryouts
+    //var stringComparison = function (a, b) {
+    //    var cost = 0;
+    //    if (a == b) return;
+    //    var vectorA = [a.length];
+    //    var vectorB = [b.length];
+    //
+    //    for (var i = 0; i < vectorA.length; i++) {
+    //        for (var j = 0; j < vectorB.length;) {
+    //            if (vectorA[i] != vectorB[j] && vectorA[i + 1] != vectorB[j]) {
+    //                cost += 1;
+    //            } else if (vectorA[i] == vectorB[j]) {
+    //                j++;
+    //            }
+    //        }
+    //    }
+    //
+    //    return cost;
+    //}
 
     $scope.searchClub = function () {
         var s = $scope.searchCriteria.toLowerCase();
 
         if (s == '') {
-            geoDataService.set([]);
+            geoDataService.setClubData([]);
             return;
         }
 
@@ -92,7 +96,7 @@ app.controller('DataCtrl', ['$scope', '$http', 'geoDataService', function ($scop
                 }
             }
         );
-        geoDataService.set($scope.keys);
+        geoDataService.setClubData($scope.keys);
     }
 }]);
 
@@ -100,24 +104,30 @@ app.controller('DataCtrl', ['$scope', '$http', 'geoDataService', function ($scop
 app.controller('MapCtrl', ['$scope', '$http', 'geoDataService', function ($scope, $http, geoDataService) {
 
     $scope.$on('onUpdate', function () {
-        $scope.clubResults = geoDataService.get();
+        $scope.clubResults = geoDataService.getClubData();
 
         initMarks();
     });
 
-    var berlinLatLng = new google.maps.LatLng(52.50, 13.34);
-    var mapCanvas = document.getElementById('mapCanvas');
-    var zoomValue = geoDataService.getZoom();
+    $scope.$on('objectClicked', function () {
+        $scope.clubObject = geoDataService.getObject();
 
+        $scope.openMarkerForObject();
+    });
+
+    // variables
+    var berlinLatLng = new google.maps.LatLng(52.50, 13.34);
+    var currentLatLng = null;
+    var mapCanvas = document.getElementById('mapCanvas');
+    var marker = new google.maps.Marker();
+    var markers = [];
     var mapOptions = {
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         center: berlinLatLng,
-        zoom: zoomValue
+        zoom: 10
     };
 
     var map = new google.maps.Map(mapCanvas, mapOptions);
-
-    var markers = [];
     var clearMap = function (map) {
         for (var i = 0; i < markers.length; i++) {
             markers[i].setMap(map);
@@ -148,10 +158,38 @@ app.controller('MapCtrl', ['$scope', '$http', 'geoDataService', function ($scope
         map.panTo(deltaLatLng);
     }
 
+    $scope.openMarkerForObject = function () {
+        var objectLatLng = new google.maps.LatLng($scope.clubObject.position.lat, $scope.clubObject.position.lng);
+
+        // content of each infoWindow
+        var content =
+            '<h1>' + $scope.clubObject.clubname + '</h1> <br>' +
+            '<em>' + 'Anschrift : ' + $scope.clubObject.address + ', ' + '</em>' + '<em> ' + $scope.clubObject.postcode + '</em> <br>' +
+            '<em>' + 'Telefon : ' + $scope.clubObject.phonenumber + '</em> </br> ' +
+            '<em>' + 'EMail-Addresse : ' + $scope.clubObject.mailaddress + '</em> <br> ' +
+            '<em>' + 'Webseite : ' + $scope.clubObject.webpage + '</em>';
+
+
+        // create the infoWindow for the marker with its content
+        var infoWindow = new google.maps.InfoWindow({
+            content: content
+        });
+
+        var marker = new google.maps.Marker({
+            position: objectLatLng,
+            map: map
+        });
+
+        // open the marker with its info
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.open(map, marker);
+        });
+
+        google.maps.event.trigger(marker, 'click');
+    }
 
     //add marker to the map
     var initMarks = function () {
-        var marker = new google.maps.Marker();
 
         clearMap(null);
         angular.forEach($scope.clubResults, function (value) {
@@ -161,15 +199,16 @@ app.controller('MapCtrl', ['$scope', '$http', 'geoDataService', function ($scope
             var content =
                 '<h1>' + value.clubname + '</h1> <br>' +
                 '<em>' + 'Anschrift : ' + value.address + ', ' + '</em>' + '<em> ' + value.postcode + '</em> <br>' +
-                '<em> ' + 'Telefon : ' + value.phonenumber + '</em> </br> ' +
-                '<em> ' + 'EMail-Addresse : ' + value.mailaddress + '</em> <br> ' +
-                '<em> ' + 'Webseite : ' + value.webpage + '</em> ';
+                '<em>' + 'Telefon : ' + value.phonenumber + '</em> </br> ' +
+                '<em>' + 'EMail-Addresse : ' + value.mailaddress + '</em> <br> ' +
+                '<em>' + 'Webseite : ' + value.webpage + '</em>';
 
             marker = new google.maps.Marker({
                 position: clubLatLng,
                 map: map,
                 title: value.clubname
             });
+
             markers.push(marker);
 
             // create the infoWindow for the marker with its content
